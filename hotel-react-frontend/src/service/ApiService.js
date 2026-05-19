925335e3d01137d6fb2ab044288559ed27233b1f
Archana <archanakamal0709@gmail.com>
2026-05-19 22:15:41 +0530

    Fixed repo structure

--- hotel-react-frontend/src/service/ApiService.js
+++ hotel-react-frontend/src/service/ApiService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import CryptoJS from "crypto-js";
 
 export default class ApiService {
-  static BASE_URL = "http://localhost:9090/api";
+  static BASE_URL = "https://hotel-booking-management-system-rbo6.onrender.com";
   static ENCRYPTION_KEY = "dennis-secrete-key";
 
   // ---------------------------
