0d71a03f4b5148b0ca9a20e3eb56b94de0b87dba
Archana <archanakamal0709@gmail.com>
2026-05-28 20:18:26 +0530

    Final Mail

--- hotel-react-frontend/src/component/booking_rooms/RoomDetailsPage.jsx
+++ hotel-react-frontend/src/component/booking_rooms/RoomDetailsPage.jsx
@@ -48,7 +48,7 @@ const RoomDetailsPage = () => {
         "template_zwbg4hm",
         {
             name: ApiService.getUser().name,
-            email: "thangaguhan7676@gmail.com",
+            email: "arunodhaya0709@gmail.com",
             room: room.type,
             checkin: checkInDate,
             checkout: checkOutDate,
